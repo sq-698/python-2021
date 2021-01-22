cbffd74c06e7f4c4e2a2242d0a551b9b24ca40eb
sq-698 <design.jadid@gmail.com>
2021-01-22 12:59:29 +0330

    initial commit

---
+++ countainer.py
@@ -0,0 +1,47 @@
+
+m = 8
+list_1 = [7, 9, 1, 2, 4, 6, 8]
+list_2 = ["m", "o", "p", "amin", "y", "c", "b"]
+
+
+my_list = [1, "Hello", 3.4, "goodbye", 8]
+print(my_list)
+my_list.append(77)
+print(my_list)
+my_list.extend(list_1)
+print(my_list)
+my_list.insert(3, "sara")
+print(my_list)
+my_list.remove(8)
+print(my_list)
+k = my_list.pop(3)
+print(my_list)
+print(k)
+IN = my_list.index(1)
+print(IN)
+CO = my_list.count(1)
+print(CO)
+list_1.sort()
+print(list_1)
+list_2.sort()
+print(list_2)
+my_list.reverse()
+print(my_list)
+list_2.clear()
+print(list_2)
+CO = my_list.copy()
+print(CO)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
